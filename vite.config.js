b12aa0117963163bab79ab48729b162ca813568e
ahump20 <197928108+ahump20@users.noreply.github.com>
2025-10-19 14:17:33 -0500

    Configure project-wide path alias

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '.'),
+    },
+  },
   server: {
     port: 3000,
     proxy: {
